c536394576f96a5910f145d0c1d85fe7173b7bc0
oroaldo <oroaldo@gmail.com>
2020-08-27 10:31:53 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+      //console.log(response.data)
+    })
+  }, []) 
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title:"REACT",
+      url:"http://www.endereco.com.br/appmobile",
+      tech: [ "ReactNative", "Flex"]
+    })
+
+    const newrepositorie = response.data
+
+    setRepositories([...repositories, newrepositorie])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete('repositories/'+id)
+
+    if (response.status == '204'){
+      const newrepositorie = repositories.filter(repositorie => repositorie.id !== id)
+      console.log(newrepositorie)
+      setRepositories(newrepositorie)
+    }
   }
 
   return (
-    <div>
+    <>  
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(dados => 
+          <li key={dados.id}>{dados.title}
+            <button onClick={() => handleRemoveRepository(dados.id)}>
+                Remover
+            </button>
+          </li>)}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+      {<button onClick={handleAddRepository}>Adicionar</button>}
+  </>
   );
 }
 
